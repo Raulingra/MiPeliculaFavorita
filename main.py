1a6a8d691341ee28e97e81b076127aba5838ec7f
Gerardo__Gramajo <gerardoquiroa04@gmail.com>
2022-05-03 09:48:50 -0600

    Creación del archivo py
    Pregunta sobre su animal favorito.

---
+++ main.py
@@ -0,0 +1,11 @@
+print('Nomnbre: Gerardo Raúl Gramajo Quiroa.')
+print('Carné: 1524222')
+print('Ingeniria en Informatica y sistemas.')
+print('URL')
+
+animalfav = input('¿Qué animal es su favortito? ')
+
+if animalfav == 'T-Rex':
+    print('Mi animal favorito es también el T-Rex')
+elif animalfav != 'T-Rex':
+    print('Ese animal es muy grande y peligroso, me encanta el T-Rex')
